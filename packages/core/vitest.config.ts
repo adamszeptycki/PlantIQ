04569912c136c78e0fae3c4a388f68d50f991ef2
Adam Szeptycki <adamszeptycki@users.noreply.github.com>
2026-01-10 17:36:33 +0100

    Initial commit

---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,20 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	plugins: [
+		tsconfigPaths({
+			loose: true,
+		}),
+	],
+	test: {
+		env: {
+			DATABASE_URL: "postgresql://postgres:@localhost:5938/kensaku-test",
+		},
+		include: ["**/*.test.ts"],
+		globals: true,
+		testTimeout: 500000,
+		setupFiles: ["./setupVitest.ts"],
+		passWithNoTests: true,
+	},
+});
